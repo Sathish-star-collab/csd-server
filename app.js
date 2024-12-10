31de5eea935ec82932a0d93d200ceb509ea9c4c0
Sathish-star-collab <sathishshankarmailto@gmail.com>
2024-12-10 18:16:03 +0530

    Update14 app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './geoip-country.dat')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 require("./config/passport.js");
