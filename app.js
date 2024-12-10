6289aee713170feef49a9e7266cdda6737a71144
Sathish-star-collab <sathishshankarmailto@gmail.com>
2024-12-10 17:48:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 require("./config/passport.js");
